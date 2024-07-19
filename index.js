56076ecfcd23d8903a9a616b3858b330323219e4
KarstenJep <kjepsen86@gmail.com>
2024-07-19 16:14:50 +0000

    added vertext ai

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from 'express';
-
 const app = express();
-l
+
+import { VertexAI } from '@google-cloud/vertexai';
+import { GoogleAuth } from 'google-auth-library';
+const auth = new GoogleAuth();
+
 app.get('/', async (req, res) => {
-    res.send('Hello world!');
+    const projectId = await auth.getProjectId();
+
+    const vertex = new VertexAI({ project: projectId });
+    const generativeModel = vertex.getGenerativeModel({
+        model: 'gemini-1.5-flash'
+    });
+
+    const animal = req.query.animal || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const resp = await generativeModel.generateContent(prompt);
+    const html = resp.response.candidates[0].content.parts[0].text;
+    res.send(html);
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+const port = parseInt(process.env.PORT) || 8081;
 app.listen(port, () => {
     console.log(`helloworld: listening on port ${port}`);
 });
-
-// update
