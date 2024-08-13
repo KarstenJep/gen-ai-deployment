152efebe4c6fc06863c4600d57502da37503a43c
KarstenJep <kjepsen86@gmail.com>
2024-08-13 19:38:44 +0000

    updated backend logic to handle ui input

--- index.js
+++ index.js
@@ -7,29 +7,24 @@ const auth = new GoogleAuth();
 
 app.use(express.json());
 app.use(express.static('public'));
-const searchWord = '';
 
-app.post('/api/keyword', (req, res) => {
-    const { keyword } = req.body;
-    console.log('Received keyword:', keyword);
-    searchWord = keyword;
-    res.json({ message: `Keyword received: ${keyword}` });
-});
+app.post('/api/keyword', async (req, res) => {
+    try {
+        const projectId = await auth.getProjectId();
+        const vertex = new VertexAI({ project: projectId });
+        const generativeModel = vertex.getGenerativeModel({
+            model: 'gemini-1.5-flash'
+        });
 
-app.get('/', async (req, res) => {
-    const projectId = await auth.getProjectId();
-
-    const vertex = new VertexAI({ project: projectId });
-    const generativeModel = vertex.getGenerativeModel({
-        model: 'gemini-1.5-flash'
-    });
-
-    // const animal = req.query.animal || 'dog';
-    // const prompt1 = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
-    const prompt = `Give me 10 fun facts about ${searchWord}. Return this as html without backticks.`
-    const resp = await generativeModel.generateContent(prompt);
-    const html = resp.response.candidates[0].content.parts[0].text;
-    res.send(html);
+        const keyword = req.query.keyword || 'dog';
+        const prompt = `Give me 10 fun facts about ${keyword}. Return this as html without backticks.`
+        const resp = await generativeModel.generateContent(prompt);
+        const html = resp.response.candidates[0].content.parts[0].text;
+        res.send(html);
+    } catch (error) {
+        console.error('Error generating content:', error);
+        res.status(500).send('Error generating content');
+    }
 });
 
 const port = parseInt(process.env.PORT) || 8081;
